d782446f37c600633622b59f68041c9a84188d05
dbsDevelops <alu.129152@usj.es>
2024-04-18 19:12:08 +0200

    Tested and added retrieving location in MainActivity

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.weatherapp
 
+import android.Manifest
 import android.animation.ObjectAnimator
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.view.animation.OvershootInterpolator
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
 import androidx.core.animation.doOnEnd
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.checkSelfPermission
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : ComponentActivity() {
+    // Initialise view model
     private val viewModel by viewModels<MainViewModel>()
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 101
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Set up splash screen
@@ -46,8 +58,31 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         // Set up main activity logic
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-        // Initialise location components
+        findViewById<Button>(R.id.btnGetLocation).setOnClickListener {
+            fetchLocation()
+        }
+    }
 
+    private fun fetchLocation() {
+        val task = fusedLocationProviderClient.lastLocation
+        if (checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+            && checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                                                    LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        task.addOnSuccessListener {
+            if (it != null) {
+                // Toast.makeText(applicationContext, "Latitude: ${it.latitude}, Longitude: ${it.longitude}", Toast.LENGTH_SHORT).show()
+                val textView = findViewById<android.widget.TextView>(R.id.tvGpsCoordinates)
+                textView.text = "Latitude: ${it.latitude}, Longitude: ${it.longitude}"
+            }
+        }
     }
 }
